aee4fca5588840e891c95f859b2fb7e650a35d57
HangPT <hangpt@ubuntu.ubuntu-domain>
2021-07-14 14:34:16 +0700

    feedbacksection sample

--- src/components/home/FeedbackSection.vue
+++ src/components/home/FeedbackSection.vue
@@ -10,191 +10,90 @@
     </div>
     <div class="page-tesmonial">
       <div id="tesmonial-slide">
-          <!-- The slideshow -->
+        <!-- The slideshow -->
         <div class="heri-container">
           <VueSlickCarousel v-bind="settings">
-          <!-- <div class="home-slick"> -->
-            <div class="group-carousel__item">
-              <div class="group-carousel__item-image row justify-content-center">
-                <img class="col-md-6" src="https://preview.colorlib.com/theme/meetme/img/personal.jpg">
+            <!-- <div class="home-slick"> -->
+            <div
+              class="group-carousel__item"
+              v-for="item in itemList"
+              :key="item.id"
+            >
+              <div
+                class="group-carousel__item-image row justify-content-center"
+              >
+                <img class="col-md-6" :src="item.img" />
               </div>
               <div class="group-carousel__item-title">
-                Emilia Bianchi
+                {{ item.name }}
               </div>
               <div class="group-carousel__item-dob">
                 14.01.2021
               </div>
               <div class="text-center group-carousel__item-intro">
-                Amazing tour, professional guide and driver, well planned tour days, diversity of destinations! All in one for a perfect trip. It was the best decision to take Upper Svaneti tour, cause mountainous parts of Georgia are really wonderful and picturesque!
+                {{ item.description }}
               </div>
             </div>
-            <div class="group-carousel__item">
-              <div class="group-carousel__item-image row justify-content-center">
-                <img class="col-md-6" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLSOffyd8vU0a8gV00R5oPUC_IYvPen6ey-OY2cQxZrtX67XtZEOR9V1L9dS7eL3zdf1U&usqp=CAU">
-              </div>
-              <div class="group-carousel__item-title">
-                Paul Richard
-              </div>
-              <div class="group-carousel__item-dob">
-                14.01.2021
-              </div>
-              <div class="text-center group-carousel__item-intro">
-                Me and my friends have been traveling around the world for such a long time and I may say, that was one of the best and impressive trips that we have ever been to. All of us are lovers of nature and it seems, Georgia has got various and a very beautiful landscape.
-              </div>
-            </div>
-            <div class="group-carousel__item">
-              <div class="group-carousel__item-image row justify-content-center">
-                <img class="col-md-6" src="https://www.tanfieldchambers.co.uk/wp-content/uploads/2018/08/Edward-Blakeney.jpg">
-              </div>
-              <div class="group-carousel__item-title">
-                Ellen Meyer
-              </div>
-              <div class="group-carousel__item-dob">
-                14.01.2021
-              </div>
-              <div class="text-center group-carousel__item-intro">
-                I still can’t keep my emotions about what I’ve seen! That was PRETTY AMAZING! What a beautiful country, what a lovely people… and Svaneti - such a wonderful place! Every single person should visit this place once in a lifetime.
-              </div>
-            </div>
-            <div class="group-carousel__item">
-              <div class="group-carousel__item-image row justify-content-center">
-                <img class="col-md-6" src="https://preview.colorlib.com/theme/meetme/img/personal.jpg">
-              </div>
-              <div class="group-carousel__item-title">
-                Emilia Bianchi
-              </div>
-              <div class="group-carousel__item-dob">
-                14.01.2021
-              </div>
-              <div class="text-center group-carousel__item-intro">
-                Amazing tour, professional guide and driver, well planned tour days, diversity of destinations! All in one for a perfect trip. It was the best decision to take Upper Svaneti tour, cause mountainous parts of Georgia are really wonderful and picturesque!
-              </div>
-            </div>
-            <div class="group-carousel__item">
-              <div class="group-carousel__item-image row justify-content-center">
-                <img class="col-md-6" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLSOffyd8vU0a8gV00R5oPUC_IYvPen6ey-OY2cQxZrtX67XtZEOR9V1L9dS7eL3zdf1U&usqp=CAU">
-              </div>
-              <div class="group-carousel__item-title">
-                Paul Richard
-              </div>
-              <div class="group-carousel__item-dob">
-                14.01.2021
-              </div>
-              <div class="text-center group-carousel__item-intro">
-                Me and my friends have been traveling around the world for such a long time and I may say, that was one of the best and impressive trips that we have ever been to. All of us are lovers of nature and it seems, Georgia has got various and a very beautiful landscape.
-              </div>
-            </div>
-            <div class="group-carousel__item">
-              <div class="group-carousel__item-image row justify-content-center">
-                <img class="col-md-6" src="https://www.tanfieldchambers.co.uk/wp-content/uploads/2018/08/Edward-Blakeney.jpg">
-              </div>
-              <div class="group-carousel__item-title">
-                Ellen Meyer
-              </div>
-              <div class="group-carousel__item-dob">
-                14.01.2021
-              </div>
-              <div class="text-center group-carousel__item-intro">
-                I still can’t keep my emotions about what I’ve seen! That was PRETTY AMAZING! What a beautiful country, what a lovely people… and Svaneti - such a wonderful place! Every single person should visit this place once in a lifetime.
-              </div>
-            </div>
-            <div class="group-carousel__item">
-              <div class="group-carousel__item-image row justify-content-center">
-                <img class="col-md-6" src="https://preview.colorlib.com/theme/meetme/img/personal.jpg">
-              </div>
-              <div class="group-carousel__item-title">
-                Emilia Bianchi
-              </div>
-              <div class="group-carousel__item-dob">
-                14.01.2021
-              </div>
-              <div class="text-center group-carousel__item-intro">
-                Amazing tour, professional guide and driver, well planned tour days, diversity of destinations! All in one for a perfect trip. It was the best decision to take Upper Svaneti tour, cause mountainous parts of Georgia are really wonderful and picturesque!
-              </div>
-            </div>
-            <div class="group-carousel__item">
-              <div class="group-carousel__item-image row justify-content-center">
-                <img class="col-md-6" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLSOffyd8vU0a8gV00R5oPUC_IYvPen6ey-OY2cQxZrtX67XtZEOR9V1L9dS7eL3zdf1U&usqp=CAU">
-              </div>
-              <div class="group-carousel__item-title">
-                Paul Richard
-              </div>
-              <div class="group-carousel__item-dob">
-                14.01.2021
-              </div>
-              <div class="text-center group-carousel__item-intro">
-                Me and my friends have been traveling around the world for such a long time and I may say, that was one of the best and impressive trips that we have ever been to. All of us are lovers of nature and it seems, Georgia has got various and a very beautiful landscape.
-              </div>
-            </div>
-            <div class="group-carousel__item">
-              <div class="group-carousel__item-image row justify-content-center">
-                <img class="col-md-6" src="https://www.tanfieldchambers.co.uk/wp-content/uploads/2018/08/Edward-Blakeney.jpg">
-              </div>
-              <div class="group-carousel__item-title">
-                Ellen Meyer
-              </div>
-              <div class="group-carousel__item-dob">
-                14.01.2021
-              </div>
-              <div class="text-center group-carousel__item-intro">
-                I still can’t keep my emotions about what I’ve seen! That was PRETTY AMAZING! What a beautiful country, what a lovely people… and Svaneti - such a wonderful place! Every single person should visit this place once in a lifetime.
-              </div>
-            </div>
-          <!-- </div> -->
-        </VueSlickCarousel>
+            <!-- </div> -->
+          </VueSlickCarousel>
         </div>
       </div>
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
-@import '@/assets/scss/main.scss';
+@import "@/assets/scss/main.scss";
 </style>
 <script>
-import VueSlickCarousel from 'vue-slick-carousel';
-import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-  // optional style for arrows & dots
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+// optional style for arrows & dots
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 export default {
-  name: "App",
+  name: "FeedbackSection",
   components: {
-    VueSlickCarousel,
+    VueSlickCarousel
   },
-  data () {
+  props: {
+    itemList: [Array]
+  },
+  data() {
     return {
       settings: {
-        "dots": true,
-        "infinite": true,
-        "speed": 300,
-        "slidesToShow": 3,
-        "slidesToScroll": 3,
-        "autoplay": true,
-        "dotsClass": 'slick-dots',
-        "prevArrow": "<a class='slick-arrow__left' href='#tesmonial-slide'><span class='nsy-icon nsy-slide-arrow-left'></span></a>",
-        "nextArrow": "<a class='slick-arrow__right' href='#tesmonial-slide'><span class='nsy-icon nsy-slide-arrow-right'></span></a>",
-        "responsive": [
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        dotsClass: "slick-dots",
+        prevArrow:
+          "<a class='slick-arrow__left' href='#tesmonial-slide'><span class='nsy-icon nsy-slide-arrow-left'></span></a>",
+        nextArrow:
+          "<a class='slick-arrow__right' href='#tesmonial-slide'><span class='nsy-icon nsy-slide-arrow-right'></span></a>",
+        responsive: [
           {
-            "breakpoint": 769,
-            "settings": {
-              "slidesToShow": 2,
-              "slidesToScroll": 2,
-              "infinite": true,
-              "dots": true,
+            breakpoint: 769,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true
             }
           },
           {
-            "breakpoint": 480,
-            "settings": {
-              "slidesToShow": 1,
-              "slidesToScroll": 1,
-              "dots": false
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: true
             }
           }
         ]
       }
-    }
+    };
   }
 };
 </script>
